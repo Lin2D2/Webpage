f7b228e286ee66ee22673fd501580dc884012bc0
Linus Behrens <linus@thebehrens.net>
2019-09-28 20:03:00 +0200

    major update

--- script/planleser.js
+++ script/planleser.js
@@ -1,13 +1,85 @@
-import request from "../require/request/request.js";
-            
-const url1 = "https://gymherderschule.de/iserv/login_check";
-const url2 = "https://gymherderschule.de/iserv/infodisplay/file/23/infodisplay/0/SchuelerOnline/subst_002.htm";
-request('http://www.google.com', function (error, response, body) {
-console.log('error:', error);
-console.log('statusCode:', response && response.statusCode); 
-console.log('body:', body);
-});
 
-// void get_plan(webpage_content) {
-// plan = webpage_content.getElementsByClassName("mon_list");
-// }
+const url_login = "https://gymherderschule.de/iserv/login_check";
+const url_today = "https://gymherderschule.de/iserv/infodisplay/file/23/infodisplay/0/SchuelerOnline/subst_002.htm";
+
+let day = 0;
+
+function sizeObj(obj) {
+    return Object.keys(obj).length;
+  }
+
+/* how to import json in java script!!!!!! */
+
+function loadJSON(callback) {   
+
+    let xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'plan.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+ function init() {
+    loadJSON(function(response) {
+     // Parse JSON string into object
+     let dict = JSON.parse(response)
+     set_plan_data(dict[day]["table"]);
+     set_title(dict[day]["title"]);
+    });
+   }
+
+init();
+
+function set_title(title) {
+    console.log(title)
+    document.getElementById("title").innerHTML = title;
+}
+
+function set_plan_data(raw_plan_data) {
+    console.log(raw_plan_data);
+    
+    for(i = 0; i < sizeObj(raw_plan_data) - 1; i+= 2) {
+        const html_plan_odd = `
+            <tr class="odd">
+                <td class="title">${raw_plan_data[i]["klasse"]}</td>
+                <td class=" tablesaw-priority-1">${raw_plan_data[i]["stunde"]}</td>
+                <td class=" tablesaw-priority-5">${raw_plan_data[i]["vertreter"]}</td>
+                <td class=" tablesaw-priority-4">${raw_plan_data[i]["fach"]}</td>
+                <td class=" tablesaw-priority-3">${raw_plan_data[i]["raum"]}</td>
+                <td class=" tablesaw-priority-2">${raw_plan_data[i]["(lehrer)"]}</td>
+                <td class=" tablesaw-priority-1">${raw_plan_data[i]["(fach)"]}</td>
+                <td class=" tablesaw-priority-2">${raw_plan_data[i]["(raum)"]}</td>
+                <td class=" tablesaw-priority-5">${raw_plan_data[i]["vertretungs-text"]}</td>
+                <td class=" tablesaw-priority-6">${raw_plan_data[i]["stattvertr.von"]}</td>
+                <td class=" tablesaw-priority-5">${raw_plan_data[i]["mitbetreuung"]}</td>
+                <td class=" tablesaw-priority-1">${raw_plan_data[i]["entfall"]}</td>
+                <td class=" tablesaw-priority-0">${raw_plan_data[i]["n.druck.(n)"]}</td>
+            </tr>`;
+        
+        const html_plan_even = `
+            <tr class="even">
+                <td class="title">${raw_plan_data[i + 1]["klasse"]}</td>
+                <td class=" tablesaw-priority-1">${raw_plan_data[i + 1]["stunde"]}</td>
+                <td class=" tablesaw-priority-5">${raw_plan_data[i + 1]["vertreter"]}</td>
+                <td class=" tablesaw-priority-4">${raw_plan_data[i + 1]["fach"]}</td>
+                <td class=" tablesaw-priority-3">${raw_plan_data[i + 1]["raum"]}</td>
+                <td class=" tablesaw-priority-2">${raw_plan_data[i + 1]["(lehrer)"]}</td>
+                <td class=" tablesaw-priority-1">${raw_plan_data[i + 1]["(fach)"]}</td>
+                <td class=" tablesaw-priority-2">${raw_plan_data[i + 1]["(raum)"]}</td>
+                <td class=" tablesaw-priority-5">${raw_plan_data[i + 1]["vertretungs-text"]}</td>
+                <td class=" tablesaw-priority-6">${raw_plan_data[i + 1]["stattvertr.von"]}</td>
+                <td class=" tablesaw-priority-5">${raw_plan_data[i + 1]["mitbetreuung"]}</td>
+                <td class=" tablesaw-priority-1">${raw_plan_data[i + 1]["entfall"]}</td>
+                <td class=" tablesaw-priority-0">${raw_plan_data[i + 1]["n.druck.(n)"]}</td>
+            </tr>`;
+
+            document.getElementById('t-body').innerHTML += html_plan_odd;
+            document.getElementById('t-body').innerHTML += html_plan_even;
+
+    };
+};
